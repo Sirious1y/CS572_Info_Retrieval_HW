63ce95b79c354b654a33a74b2ac98b5b1ba35aa9
Sirious1y <jamess0378@gmail.com>
2024-02-18 21:33:57 -0500

    Indexing

--- P1/index.py
+++ P1/index.py
@@ -1,15 +1,17 @@
 import os
 import pyterrier as pt
 
-# os.environ["JAVA_HOME"] = "/Program Files/Common Files/Java/jdk-21/"
 pt.init()
 
-files = pt.io.find_files("./text/ft/")
+text_files = ['ft', 'fbis', 'latimes']
 
-index_folder = "C:/MyThing/Emory/2024/CS572/P1/ft_index/"
-index_path = os.path.abspath(index_folder)
+for file_name in text_files:
+    files = pt.io.find_files("./text/" + file_name + '/')
 
-print(files)
+    index_path = "E:/Emory/2024/CS572/P1/" + file_name + "_index/"
+    index_path = os.path.abspath(index_path)
 
-indexer = pt.TRECCollectionIndexer(index_path, verbose=True, blocks=False)
-indexref = indexer.index(files)
+    # dataset = pt.get_dataset("trec-robust-2004")
+
+    indexer = pt.TRECCollectionIndexer(index_path, verbose=True, blocks=False)
+    indexref = indexer.index(files)
